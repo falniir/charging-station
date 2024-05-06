ef7c0da21f2fd59385246204b42466d29a35507f
Håkon Røskaft <haakoero@stud.ntnu.no>
2024-05-06 13:34:10 +0200

    Removed app-backend folder

--- app-backend/backend/views.py
+++
@@ -1,8 +0,0 @@
-import json
-from django.http import JsonResponse
-from backend.mqtt import client as mqtt_client
-
-def publish_message(request):
-    request_data = json.loads(request.body)
-    rc, mid = mqtt_client.publish(request_data['topic'], request_data['msg'])
-    return JsonResponse({'code': rc})
