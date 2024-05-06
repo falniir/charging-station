299b3b7081f54f2044a6a7f43347a781cbcc2578
Håkon Røskaft <haakoero@stud.ntnu.no>
2024-05-06 12:03:54 +0200

    Added csrf ignore to mqtt view in django backend

--- charging-backend/app/views.py
+++ charging-backend/app/views.py
@@ -1,7 +1,11 @@
 import json
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from app.mqtt import client as mqtt_client
 
+# Added csrf exemption since we will only use localy run brokers for the demo
+# TODO change into proper csrf handling
+@csrf_exempt
 def publish_message(request):
     request_data = json.loads(request.body)
     rc, mid = mqtt_client.publish(request_data['topic'], request_data['msg'])
