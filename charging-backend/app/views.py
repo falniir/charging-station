e3474695f2ccd6bda391c60d03bbc1b27f25de46
Håkon Røskaft <haakoero@stud.ntnu.no>
2024-05-06 14:39:43 +0200

    Updated app/views to work with the correct requests

--- charging-backend/app/views.py
+++ charging-backend/app/views.py
@@ -4,7 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 from app.mqtt import client as mqtt_client
 
-# RESERVE MAINTANENCE START
+# RESERVE MAINTANENCE START RESET
 # Added csrf exemption since we will only use localy run brokers for the demo
 # TODO change into proper csrf handling
 @csrf_exempt
@@ -12,8 +12,6 @@ def publish_message(request):
     request_data = json.loads(request.body)
     msg = request_data['msg']
     rc, mid = -1, -1
-    if ((msg == 'RESERVE') or (msg == 'MAINTANENCE') or(msg == 'START')):
-        rc, mid = mqtt_client.publish(settings.MQTT_TOPIC, request_data['msg'])
-    else:
-        something = 'else'    
+    if ((msg == 'RESERVE') or (msg == 'MAINTANENCE') or (msg == 'START') or (msg == 'RESET') or (msg == 'STOP')):
+        rc, mid = mqtt_client.publish(settings.MQTT_TOPIC, request_data['msg']) 
     return JsonResponse({'code': rc, 'mid': mid})
